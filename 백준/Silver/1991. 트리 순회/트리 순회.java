249dc1b2348e58e6ac222fa7d4dea4522ddf01b8
Tony <86696759+capDoYeonLee@users.noreply.github.com>
2025-01-15 14:34:19 +0900

    [Silver I] Title: 트리 순회, Time: 108 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Scanner;
 
 class Node {
     String data;
@@ -22,24 +24,17 @@ class Tree {
         return root;
     }
 
-    public Node makeNode(Node left, String data, Node right) {
-        Node node = new Node(data);
-        node.left = left;
-        node.right = right;
-        return node;
-    }
-
     public void inOrder(Node node) {
         if (node != null) {
             inOrder(node.left);
-            System.out.print(node.data + "");
+            System.out.print(node.data);
             inOrder(node.right);
         }
     }
-    
+
     public void preOrder(Node node) {
         if (node != null) {
-            System.out.print(node.data + "");
+            System.out.print(node.data);
             preOrder(node.left);
             preOrder(node.right);
         }
@@ -49,47 +44,40 @@ class Tree {
         if (node != null) {
             postOrder(node.left);
             postOrder(node.right);
-            System.out.print(node.data + "");
+            System.out.print(node.data);
         }
     }
 }
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 노드 수
-        sc.nextLine(); // 버퍼 비우기
+    public static void main(String[] args) throws IOException {
 
-        // 노드를 저장할 맵
-        HashMap<String, Node> map = new HashMap<>();
-
-        // 트리 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        HashMap<String, Node> nodeMap = new HashMap<>();
         Tree tree = new Tree();
-
-        // 입력 처리
+        
         for (int i = 0; i < n; i++) {
-            String[] input = sc.nextLine().split(" ");
-            String parent = input[0];
+            String[] input = br.readLine().split(" ");
+            String root = input[0];
             String left = input[1];
             String right = input[2];
-
-            // 부모 노드 생성/조회
-            map.putIfAbsent(parent, new Node(parent));
-            Node parentNode = map.get(parent);
-
-            // 왼쪽 자식 노드 생성
+            
+            
+            nodeMap.putIfAbsent(root, new Node(root));
+            Node parentNode = nodeMap.get(root);
+            
             if (!left.equals(".")) {
-                map.putIfAbsent(left, new Node(left));
-                parentNode.left = map.get(left);
+                nodeMap.putIfAbsent(left, new Node(left));
+                parentNode.left = nodeMap.get(left);
             }
-
-            // 오른쪽 자식 노드 생성
+            
             if (!right.equals(".")) {
-                map.putIfAbsent(right, new Node(right));
-                parentNode.right = map.get(right);
+                nodeMap.putIfAbsent(right, new Node(right));
+                parentNode.right = nodeMap.get(right);
             }
-
-            // 루트 설정
+            
             if (i == 0) {
                 tree.setRoot(parentNode);
             }
@@ -100,6 +88,6 @@ public class Main {
         tree.inOrder(tree.getRoot());
         System.out.println();
         tree.postOrder(tree.getRoot());
-        
+
     }
 }
